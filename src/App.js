335e329bac5e66d0cd7dda4bb59e88373bec2194
nchoin <139597297+nchoin@users.noreply.github.com>
2024-06-24 21:45:42 -0400

    added footer to App.js to include links to Git Hub, the repo and deployed site

--- src/App.js
+++ src/App.js
@@ -7,6 +7,33 @@ export default function App() {
     <div className="Search">
       <h1 className="weatherApp">Weather App</h1>
       <SearchWeatherApp />
+      <p className="aboutFooter">
+        This project was coded by{" "}
+        <a
+          href="https://github.com/nchoin"
+          title="link to GitHub profile"
+          className="linkFooter"
+        >
+          Nicole Choiniere-Kroeker
+        </a>
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/nchoin/weather-react"
+          title="link to repo on GitHub"
+          className="linkFooter"
+        >
+          GitHub{" "}
+        </a>
+        and hosted by{" "}
+        <a
+          href="https://weather-react-shecodes-nck.netlify.app/"
+          title="weather app using react"
+          className="linkFooter"
+        >
+          {" "}
+          Netlify.
+        </a>
+      </p>
     </div>
   );
 }
